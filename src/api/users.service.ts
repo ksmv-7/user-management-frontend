5b142be33af676e74cae66423d99b64a6740dc3b
kirilsimv <kiril.simeonov@sagecyber.com>
2025-01-15 03:21:15 +0200

    change endpoint

--- src/api/users.service.ts
+++ src/api/users.service.ts
@@ -2,7 +2,7 @@ import { BACKEND_URL } from "../consts";
 import { User } from "../types/user/types";
 
 export const fetchUsers = async ({ pageParam = 1, limit = 3 }) => {
-  const response = await fetch(`${BACKEND_URL}/users?page=${pageParam}&limit=${limit}`);
+  const response = await fetch(`${BACKEND_URL}/users/by-page?page=${pageParam}&limit=${limit}`);
   const data = await response.json();
   return {
     users: data.userData as User[],
